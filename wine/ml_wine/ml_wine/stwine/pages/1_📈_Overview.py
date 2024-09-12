2aaa996f468e93bffba65563ff7e03992b7528e8
Huda Febrianto Nurrohman <febriantohuda@gmail.com>
2024-09-12 15:21:11 +0700

    new : add feature correlation heatmap

--- wine/ml_wine/ml_wine/stwine/pages/1_📈_Overview.py
+++ wine/ml_wine/ml_wine/stwine/pages/1_📈_Overview.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import pandas as pd
 import streamlit as st
+import plotly.figure_factory as ff
+import plotly.graph_objs as go
 
 st.set_page_config(page_title="Overview", page_icon="📊")
 
@@ -17,6 +19,44 @@ st.write(
 def get_dataset() -> pd.DataFrame:
     return pd.read_csv('ml_wine/data/train.csv')
 
+
+@st.cache_data
+def get_correlation_heatmap(df: pd.DataFrame) -> go.Figure:
+    corr = np.round(df.corr(), 2)
+    mask = np.triu(np.ones_like(corr, dtype = bool))
+    c_mask = np.where(~mask, corr, 100)
+
+    c = []
+    for i in c_mask.tolist()[1:]:
+        c.append([x for x in i if x != 100])
+
+    fig = ff.create_annotated_heatmap(
+        z=c[::-1],
+        x=corr.index.tolist()[:-1],
+        y=corr.columns.tolist()[1:][::-1],
+        colorscale='bluyl',
+    )
+
+    fig.update_layout(
+        title = {'text': '<b>Feature Correlation <br> <sup>Heatmap</sup></b>'},
+        height = 650, width = 650,
+        margin = dict(t=210, l = 80),
+        template = 'simple_white',
+        yaxis = dict(autorange = 'reversed'),
+    )
+
+    fig.add_trace(
+        go.Heatmap(
+            z=c[::-1],
+            colorscale='bluyl',
+            showscale=True,
+            visible=False,
+        )
+    )
+    
+    fig.data[1].visible = True
+
+    return fig
 
 # status_text = st.sidebar.empty()
 # progress_bar = st.sidebar.progress(0)
@@ -37,3 +77,6 @@ st.dataframe(df.head(), hide_index=True)
 '''## Statistics'''
 st.dataframe(df.describe().T)
 
+'''## Correlation'''
+fig = get_correlation_heatmap(df)
+st.plotly_chart(fig, use_container_width=True)
